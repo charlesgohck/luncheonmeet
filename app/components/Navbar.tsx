afbd15d771a6c8641f2455fae211955e1624ab99
charlesgohck <15852016+charlesgohck@users.noreply.github.com>
2024-11-17 17:13:39 +0800

    [AUTH] Buggy implementation of sign in button without optimizations

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,10 @@
-export function NavBar() {
+import { auth, signIn } from "@/auth"
+
+export async function NavBar() {
+
+    const session = await auth();
+    console.log(session);
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -30,27 +36,43 @@ export function NavBar() {
                 <a className="btn btn-ghost text-xl">LuncheonMeet</a>
             </div>
             <div className="navbar-end">
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                {
+                    session && session?.user ? (
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Profile Photo from SSO"
+                                        src={session.user.image ? session.user.image : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} 
+                                    />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li><a>Logout</a></li>
+                            </ul>
                         </div>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
+                    ) : (
+                        <div>
+                            <form
+                                action={async () => {
+                                    "use server"
+                                    await signIn("google")
+                                }}
+                            >
+                                <button className="btn btn-primary" type="submit">Sign In with Google</button>
+                            </form>
+
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
