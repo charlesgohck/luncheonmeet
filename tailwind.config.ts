2b7945872bf133970304f0c48de5ce192c087e50
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2025-03-09 17:39:48 +0800

    [TAILWIND] Removed tailwind daisyui imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,5 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [require('daisyui'), require("@tailwindcss/typography")],
+  ]
 } satisfies Config;
