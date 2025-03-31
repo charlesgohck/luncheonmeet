3f987579f1a3ba5268e9ef15d1e0e2ff5fb5b2d5
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2025-03-31 16:28:22 +0800

    [ANALYTICS] Removed vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <Analytics/>
-            <SpeedInsights/>
             <body>{children}</body>
         </html>
     );
